0fa6b3e9f171c91230e8ff1f4600ad0ed57d5d49
everval <eduardo.vera@gmail.com>
2023-10-10 22:12:36 +0200

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,9 @@ using Test
     @test fracdiff(ones(10,1),0.0) ≈ ones(10,1)
     @test fracdiff(ones(3,1),0.5) ≈ [1; 0.5; 0.375]
     @test csadiff(zeros(10,1),1.4,1.4) ≈ zeros(10,1)
+    @test arfimagen(10,1,[],0,[];σ=0) ≈ zeros(10,1)
+    @test arfigen(10,0,[],0;σ=0) ≈ zeros(10,1)
+    @test arfigen(10,1,[],0;σ=0) ≈ ones(10,1)
+    @test figen(10,0;σ=0) ≈ zeros(10,1)
 
 end
