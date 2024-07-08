9d91309a8f2a7ca48b7225bf72e594ddcf7f7a38
everval <eduardo.vera@gmail.com>
2024-07-08 15:17:06 +0200

    Started structural changes function

--- src/LongMemory.jl
+++ src/LongMemory.jl
@@ -58,4 +58,10 @@ include("Forecasters.jl")
 using .Forecasters
 export fi_ar_coefs, fi_forecast, fi_forecast_plot, csa_forecast, csa_forecast_plot, har_forecast, har_forecast_plot
 
+include("StructuralChanges.jl")
+using .StructuralChanges
+export lm_change_test
+
+
+
 end
---
+++ src/StructuralChanges.jl
@@ -0,0 +1,33 @@
+"""
+# StructuralChanges
+
+This module contains functions to estimate structural changes in models contaning long memory errors.
+
+## Author
+
+[J. Eduardo Vera-Valdés](https://everval.github.io/)
+
+"""
+module StructuralChanges
+
+include("LogPeriodEstimators.jl")
+import .LogPeriodEstimators: exact_whittle_est
+
+include("GeneratingFunctions.jl")
+import .GeneratingFunctions: fracdiff
+
+export lm_change_test
+
+
+function lm_change_test(y::Array)
+    T = length(y)
+
+    # Estimate the long memory parameter
+    d0 = exact_whittle_est(y)
+
+    # Integrating the series
+    x = fracdiff(y, d0)
+
+    return x
+
+end
